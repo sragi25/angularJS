b90a9d089de6d3e96ab0fc6fd244fdbfecc365f5
Swapna <csrangu@gmail.com>
2019-03-04 09:46:11 -0600

    Promise Part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,9 +23,12 @@ export class HomeComponent implements OnInit {
     private leaderService: LeaderService) { }
 
   ngOnInit() {
-    this.dish = this.dishservice.getFeaturedDish();
-    this.promotion = this.promotionService.getFeaturedPromotion();
-    this.leaders = this.leaderService.getFeaturedLeader();
+    this.dishservice.getFeaturedDish()
+    .then(dish => this.dish = dish);
+    this.promotionService.getFeaturedPromotion()
+    .then(promotion => this.promotion = promotion);
+    this.leaderService.getFeaturedLeader()
+    .then(leader => this.leaders = leader);
   }
 
 }
